0cbe72d145ae8673b4bd8dc977ee44f955d8c651
Anton Krichevskii <kriant@hey.com>
2022-11-04 12:06:46 +0200

    feat(): language selector save locale in localstorage

--- src/lib/translations/LanguageSelector.svelte
+++ src/lib/translations/LanguageSelector.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+	import type { browser } from '$app/environment'
 	import { locale, locales } from 'svelte-i18n'
+	import { browser } from '$app/environment'
 
-	$: currentLocale = $locale.split('-')[0]
+	let currentLocale: string
+
+	if (browser) {
+		const storedLocale = localStorage.getItem('locale')
+		if (storedLocale) {
+			$locale = storedLocale
+		}
+	}
+
+	$: {
+		currentLocale = $locale.split('-')[0]
+		if (browser) {
+			localStorage.setItem('locale', currentLocale)
+		}
+	}
 </script>
 
 <select class="select" bind:value={$locale}>
